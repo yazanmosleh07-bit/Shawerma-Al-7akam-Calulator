c1247286785fa56f7c80d137db6b5e95606f5931
yazanmosleh07-bit <yazanmosleh07@gmail.com>
2026-04-24 03:53:00 +0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,24 @@
+/*  
+>>Shawerma Al 7akam calculator<<
+>>User will chose what shawerma meal they want to use as they’re reference of calculation 
+>>they chose an metric(time,days,distance, etc)
+>>they input the value of the metric they want to convert
+>>the program will calculate the equivalent amount of shawerma meals to the desierd metric and output the result to the user
+*/
+
+
+
+
+
+public class App {
+
+    public static void main(String[] args) {
+        
+    }
+
+
+
+
+
+    
+}
